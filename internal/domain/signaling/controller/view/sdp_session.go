a9a1d7c78bf2afcb44be6c76ac4134a0851f202f
Maxim Tretyakov <tretiakov.m@bookcentre.ru>
2025-12-16 23:14:08 +0300

    [fix-coturn-credentials] Поправил генерацию кредов coturn и маршалинг кандидатов

--- internal/domain/signaling/controller/view/sdp_session.go
+++ internal/domain/signaling/controller/view/sdp_session.go
@@ -1,7 +1,14 @@
 package view
 
 type SDPData struct {
-	Type      string      `json:"type"`
-	SDP       string      `json:"sdp,omitempty"`
-	Candidate interface{} `json:"candidate,omitempty"`
+	Type      string       `json:"type"`
+	SDP       string       `json:"sdp,omitempty"`
+	Candidate IceCandidate `json:"candidate,omitempty"`
+}
+
+type IceCandidate struct {
+	Candidate        string `json:"candidate"`
+	SdpMid           string `json:"sdpMid"`
+	SdpMLineIndex    int    `json:"sdpMLineIndex"`
+	UsernameFragment string `json:"usernameFragment,omitempty"`
 }
